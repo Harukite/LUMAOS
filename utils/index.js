f08b90957db70ae5d0727eea4b31c758399964f3
Haruki <jaychougo@gmail.com>
2025-02-18 10:40:27 +0800

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -19,49 +19,27 @@ async function readAccounts() {
   }
 }
 
-// 解析代理 URL
-function parseProxyUrl(proxyUrl) {
-  try {
-    const parsedUrl = new URL(proxyUrl);
-    const protocol = parsedUrl.protocol.replace(":", "");
-    const [username, password] = parsedUrl.username
-      ? parsedUrl.username.split(":")
-      : [null, null];
-
-    return {
-      protocol,
-      host: parsedUrl.hostname,
-      port: parseInt(parsedUrl.port),
-      username,
-      password,
-    };
-  } catch (error) {
-    log.error("代理 URL 解析错误:", error);
-    return null;
-  }
-}
-
 // 创建代理 Agent
 function createProxyAgent(proxyUrl) {
-  const proxyConfig = parseProxyUrl(proxyUrl);
-  if (!proxyConfig) return null;
-
   try {
-    switch (proxyConfig.protocol) {
-      case "socks4":
-      case "socks5":
-        return new SocksProxyAgent(proxyUrl);
-      case "http":
-      case "https":
-        return new HttpsProxyAgent(proxyUrl);
-      default:
-        log.warn(`不支持的代理协议: ${proxyConfig.protocol}`);
-        return null;
+     if (proxyUrl) {
+    if (proxyUrl.startsWith("http://")) {
+      return new HttpsProxyAgent(proxyUrl);
+    } else if (
+      proxyUrl.startsWith("socks4://") ||
+      proxyUrl.startsWith("socks5://")
+    ) {
+      return new SocksProxyAgent(proxyUrl);
+    } else {
+      log.warn(`Unsupported proxy type: ${proxyUrl}`);
+      return null;
     }
+  }
+  return null;
   } catch (error) {
     log.error("创建代理 Agent 错误:", error);
     return null;
   }
 }
 
-export { readAccounts, parseProxyUrl, createProxyAgent };
+export { readAccounts,createProxyAgent };
